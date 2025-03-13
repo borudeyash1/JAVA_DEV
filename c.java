7f8547f347c996131554b23155ef8cffca2ce425
borudeyash1 <borudeyash12@gmail.com>
2025-03-13 13:59:12 +0530

    yasdf

--- c.java
+++ c.java
@@ -1,8 +1,25 @@
 import pack.*;
-class c{
-public static void main(String[] args)
-{
-   pack.A obj = new pack.A();
-   obj.msg();
-}
+
+
+
+public class c {
+
+    public static void main(String[] args) {
+
+        pack.A obj = new pack.A();
+
+        obj.msg();
+
+        obj.msg2();
+
+        obj.circle();
+
+        obj.triangle();
+
+        //show result for some time
+
+        
+
+    }
+
 }
--- pack/A.java
+++ pack/A.java
@@ -1,7 +1,78 @@
 package pack;
-public class A
-{
-public void msg(){
-System.out.println("Welcome ,this is a package");
-}
-}
+
+import java.util.*;
+
+
+
+
+
+public class A {
+
+    public void msg() {
+    
+        System.out.println("This is a package");
+        
+        }
+        
+        
+        
+    public void msg2() {
+        
+            System.out.println("This is another message");
+            
+            }
+            
+            
+            
+    public void msg3() {
+        
+            System.out.println("This is yet another message");
+            
+            } 
+                
+    public void circle(){
+    
+      //measuring are and perimeter of circle by user
+        
+      Scanner sc = new Scanner(System.in);
+      
+      System.out.println("Enter the radius of the circle");
+                
+      double r = sc.nextDouble();
+      
+          double area = 3.14*r*r;
+          
+              double perimeter = 2*3.14*r;
+              
+                  System.out.println("The area of the circle is "+area);
+                  
+                      System.out.println("The perimeter of the circle is "+perimeter);
+                      
+                                    
+                                    
+                                    
+                                    
+      }
+                                                
+      public void triangle(){
+      
+          //measuring area and perimeter of triangle by user
+          
+              Scanner sc = new Scanner(System.in);
+              
+        System.out.println("Enter the base of the triangle");
+        
+            double b = sc.nextDouble();
+            
+        
+        System.out.println("Enter the height of the triangle");
+        
+            double h = sc.nextDouble();
+            
+                double area = 0.5*b*h;  
+                
+                    System.out.println("The area of triangle is: "+area);
+                    
+                    }
+                    
+                                                  }
